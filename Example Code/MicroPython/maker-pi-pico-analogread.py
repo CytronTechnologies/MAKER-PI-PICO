7d832a256dd00f923010cdb3134a9eeab42ba5a7
sclim <kiate.lim@gmail.com>
2021-02-04 15:07:23 +0800

    Added comments

--- Example Code/MicroPython/maker-pi-pico-analogread.py
+++ Example Code/MicroPython/maker-pi-pico-analogread.py
@@ -1,9 +1,9 @@
 import machine
 import time
 
-potentiometer = machine.ADC(26)
+potentiometer = machine.ADC(26)     # set GP26 as analog input pin
 
 while True:
-    print(potentiometer.read_u16())
-    time.sleep_ms(50)
+    print(potentiometer.read_u16()) # print analog value to serial
+    time.sleep_ms(50)               # sleep for 50ms, then repeat.
     
