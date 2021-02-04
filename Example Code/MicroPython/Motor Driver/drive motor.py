aac8a8e9bd15221e4603c15195d7ba04bedaafc5
bhavithiran97 <bhavithiran97@gmail.com>
2021-02-04 11:24:51 +0800

    Control Maker Drive using Maker Pico

---
+++ Example Code/MicroPython/Motor Driver/drive motor.py
@@ -0,0 +1,16 @@
+from motor_driver import *         # import motor_driver library
+
+# Drive Motor
+
+motor = motor_driver(4,5,2,3)      # M1A = 4, M1B = 5, M2A = 2, M2B = 3
+
+motor.speed(50,50)                 # move forward at speed 50
+utime.sleep(5)                     # sleep 5 second
+
+motor.speed(0,50)                  # turn left at speed 50
+utime.sleep(5)
+
+motor.speed(50,0)                  # turn right at speed 50
+utime.sleep(5)
+
+motor.brake()                      # brake the motor
---
+++ Example Code/MicroPython/Motor Driver/motor_driver.py
@@ -0,0 +1,41 @@
+import machine
+import utime
+
+M1A = machine.PWM(machine.Pin(4))
+M1B = machine.PWM(machine.Pin(5))
+M2A = machine.PWM(machine.Pin(2))
+M2B = machine.PWM(machine.Pin(3))
+
+
+class motor_driver():
+    def __init__(self,M1A,M1B,M2A,M2B):
+        
+        self.M1A = machine.PWM(machine.Pin(M1A))
+        self.M1B = machine.PWM(machine.Pin(M1B))
+        self.M2A = machine.PWM(machine.Pin(M2A))
+        self.M2B = machine.PWM(machine.Pin(M2B))
+    
+    # Will return a integer
+    def convert(self,x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+    def speed(self,speedLeft, speedRight):
+        speedLeft = self.convert(speedLeft,0,100,0,65534)
+        speedRight = self.convert(speedRight,0,100,0,65534)
+        if speedLeft > 0:
+            self.M1A.duty_u16(speedLeft)
+            self.M1B.duty_u16(0)
+        else:
+            self.M1A.duty_u16(0)
+            self.M1B.duty_u16(abs(speedLeft))
+
+        if speedRight > 0:
+            self.M2A.duty_u16(speedRight)
+            self.M2B.duty_u16(0)
+        else:
+            self.M2A.duty_u16(0)
+            self.M2B.duty_u16(abs(speedRight))
+            
+    def brake(self):
+        self.speed(0,0)
+        
