5c6ab7586c82a6c57a2a45a3683b5faa438605e1
sclim <kiate.lim@gmail.com>
2021-04-02 16:25:36 +0800

    commit example

---
+++ Example Code/MicroPython/maker-pi-pico-ultrasound-hcsr04.py
@@ -0,0 +1,25 @@
+from machine import Pin
+import utime
+
+trigger = Pin(7, Pin.OUT)
+echo = Pin(6, Pin.IN)
+
+distance = 0
+def ultrasound():
+    global distance
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(5)
+    trigger.low()
+    while echo.value() == 0:
+        signaloff = utime.ticks_us()
+    while echo.value() == 1:
+        signalon = utime.ticks_us()
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+
+while True:
+    ultrasound()
+    print("The distance from object is ", distance, "cm")
+    utime.sleep(1)
