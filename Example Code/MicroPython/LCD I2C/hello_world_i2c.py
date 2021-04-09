eedc9e740e0ae11f17a5769de8170c80bfa5241b
bhavithiran97 <bhavithiran97@gmail.com>
2021-04-09 11:30:28 +0800

    Added reference

--- Example Code/MicroPython/LCD I2C/hello_world_i2c.py
+++ Example Code/MicroPython/LCD I2C/hello_world_i2c.py
@@ -1,3 +1,6 @@
+###  This example code uses: 3V3 I2C and SPI 1602 Serial Character LCD ;; Reference: https://my.cytron.io/p-3v3-i2c-and-spi-1602-serial-character-lcd
+###  Make sure your LCD is 3.3V compatible
+
 from LCD_I2C import *    #import LCD_I2C library
 
 # I2C LCD Display
