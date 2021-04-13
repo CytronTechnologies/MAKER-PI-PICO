40dd87612cee0b6e504bd74f9de0e1b301293703
bhavithiran97 <bhavithiran97@gmail.com>
2021-04-13 12:33:06 +0800

    Update pico-servo-control.py

--- Example Code/MicroPython/pico-servo-control.py
+++ Example Code/MicroPython/pico-servo-control.py
@@ -1,3 +1,6 @@
+###  This example code uses: Maker Pi Pico ;; Reference: www.cytron.io/p-maker-pi-pico
+###  This example code uses: Analog Micro Servo 9g (3V-6V) ;; Reference: www.cytron.io/p-analog-micro-servo-9g-3v-6v
+
 from machine import Pin, PWM
 import time
 
@@ -5,11 +8,11 @@ import time
 MIN_DUTY = 1600
 MAX_DUTY = 8400
 
-pwm = PWM(Pin(1))
-pwm.freq(50)
+pwm = PWM(Pin(1))     # Connect servo at pin 1
+pwm.freq(50)          # Set PWM frequency to 50Hz
 
 while True:
-    pwm.duty_u16(MIN_DUTY)
+    pwm.duty_u16(MIN_DUTY)    # Set duty cycle
     time.sleep_ms(1000)
     pwm.duty_u16(MAX_DUTY)
     time.sleep_ms(1000)
