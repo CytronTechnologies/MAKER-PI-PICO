6bddc4786c3dfd625beb6615d79ed8b0f7ea0e52
bhavithiran97 <bhavithiran97@gmail.com>
2021-04-13 09:45:17 +0800

    Update hello_world_spi.py

--- Example Code/MicroPython/LCD SPI/hello_world_spi.py
+++ Example Code/MicroPython/LCD SPI/hello_world_spi.py
@@ -5,6 +5,6 @@ from LCD_SPI import *    #import LCD_SPI library
 
 # SPI LCD Display
 
-lcd = LCD(sck=2, scl=3)  # Create LCD object with LCD's sck pin connected to PICO's sck pin 2, LCD's sda pin connected to Pico's tx pin 3
+lcd = LCD(sck=2, tx=3, cs=5)  # Create LCD object with LCD's sck pin connected to PICO's sck pin 2, LCD's sda pin connected to Pico's tx pin 3, LCD's cs pin connected to Pico's CSn pin 5
 lcd.set_cursor(0,0)          # Set the cursor at first column, first row
 lcd.write("Hello World")     # Write string to the LCD   
