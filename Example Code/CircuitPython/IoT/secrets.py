e0a793680aaeed8737261dd7f5a46bbbdf43e499
Idris Zainal Abidin <idris@cytron.com.my>
2021-07-30 14:49:11 +0800

    Update secrets.py

--- Example Code/CircuitPython/IoT/secrets.py
+++ Example Code/CircuitPython/IoT/secrets.py
@@ -6,5 +6,7 @@ secrets = {
     "blynk_auth_token"         : "my_blynk_auth_token",
     "thingspeak_write_api_key" : "my_thingspeak_write_api_key",
     "telegram_bot_token"       : "my_telegram_bot_token",
-    "telegram_chat_id"         : "my_telegram_chat_id"
+    "telegram_chat_id"         : "my_telegram_chat_id",
+    "aio_username"             : "my_adafruitio_username",
+    "aio_key"                  : "my_adafruitio_key"
 }
