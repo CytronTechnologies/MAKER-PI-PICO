4402b9f727ab1d8c044a857d4deaed11cc42eb14
Luqmanul Hakim Zulkifli <49841356+mlhakimz@users.noreply.github.com>
2021-10-13 17:47:35 +0800

    Update pyRTOS_blink.py

--- Example Code/MicroPython/pyRTOS/pyRTOS_blink.py
+++ Example Code/MicroPython/pyRTOS/pyRTOS_blink.py
@@ -1,8 +1,7 @@
 import machine
-import utime
 import pyRTOS
 
-def led1(self):
+def task1(self):
     ledpin1 = machine.Pin(0, machine.Pin.OUT)
     ledpin1.value(0)
     yield
@@ -11,7 +10,7 @@ def led1(self):
         ledpin1.toggle()
         yield [pyRTOS.timeout(1)]
 
-def led2(self):
+def task2(self):
     ledpin2 = machine.Pin(1, machine.Pin.OUT)
     ledpin2.value(0)
     yield
@@ -20,7 +19,7 @@ def led2(self):
         ledpin2.toggle()
         yield [pyRTOS.timeout(0.95)]
     
-def led3(self):
+def task3(self):
     ledpin3 = machine.Pin(2, machine.Pin.OUT)
     ledpin3.value(0)
     yield
@@ -29,7 +28,7 @@ def led3(self):
         ledpin3.toggle()
         yield [pyRTOS.timeout(0.90)]
 
-def led4(self):
+def task4(self):
     ledpin4 = machine.Pin(3, machine.Pin.OUT)
     ledpin4.value(0)
     yield
@@ -38,7 +37,7 @@ def led4(self):
         ledpin4.toggle()
         yield [pyRTOS.timeout(0.85)]
 
-def led5(self):
+def task5(self):
     ledpin5 = machine.Pin(4, machine.Pin.OUT)
     ledpin5.value(0)
     yield
@@ -47,7 +46,7 @@ def led5(self):
         ledpin5.toggle()
         yield [pyRTOS.timeout(0.80)]
 
-def led6(self):
+def task6(self):
     ledpin6 = machine.Pin(5, machine.Pin.OUT)
     ledpin6.value(0)
     yield
@@ -56,7 +55,7 @@ def led6(self):
         ledpin6.toggle()
         yield [pyRTOS.timeout(0.75)]
 
-def led7(self):
+def task7(self):
     ledpin7 = machine.Pin(6, machine.Pin.OUT)
     ledpin7.value(0)
     yield
@@ -65,7 +64,7 @@ def led7(self):
         ledpin7.toggle()
         yield [pyRTOS.timeout(0.70)]
 
-def led8(self):
+def task8(self):
     ledpin8 = machine.Pin(7, machine.Pin.OUT)
     ledpin8.value(0)
     yield
@@ -74,7 +73,7 @@ def led8(self):
         ledpin8.toggle()
         yield [pyRTOS.timeout(0.65)]
 
-def led9(self):
+def task9(self):
     ledpin9 = machine.Pin(8, machine.Pin.OUT)
     ledpin9.value(0)
     yield
@@ -84,7 +83,7 @@ def led9(self):
         yield [pyRTOS.timeout(0.60)]
 
 
-def led10(self):
+def task10(self):
     ledpin10 = machine.Pin(9, machine.Pin.OUT)
     ledpin10.value(0)
     yield
@@ -93,7 +92,7 @@ def led10(self):
         ledpin10.toggle()
         yield [pyRTOS.timeout(0.55)]
 
-def led11(self):
+def task11(self):
     ledpin11 = machine.Pin(10, machine.Pin.OUT)
     ledpin11.value(0)
     yield
@@ -102,7 +101,7 @@ def led11(self):
         ledpin11.toggle()
         yield [pyRTOS.timeout(0.50)]
 
-def led12(self):
+def task12(self):
     ledpin12 = machine.Pin(11, machine.Pin.OUT)
     ledpin12.value(0)
     yield
@@ -111,7 +110,7 @@ def led12(self):
         ledpin12.toggle()
         yield [pyRTOS.timeout(0.45)]
 
-def led13(self):
+def task13(self):
     ledpin13 = machine.Pin(12, machine.Pin.OUT)
     ledpin13.value(0)
     yield
@@ -120,7 +119,7 @@ def led13(self):
         ledpin13.toggle()
         yield [pyRTOS.timeout(0.40)]
 
-def led14(self):
+def task14(self):
     ledpin14 = machine.Pin(13, machine.Pin.OUT)
     ledpin14.value(0)
     yield
@@ -129,7 +128,7 @@ def led14(self):
         ledpin14.toggle()
         yield [pyRTOS.timeout(0.35)]
 
-def led15(self):
+def task15(self):
     ledpin15 = machine.Pin(14, machine.Pin.OUT)
     ledpin15.value(0)
     yield
@@ -138,7 +137,7 @@ def led15(self):
         ledpin15.toggle()
         yield [pyRTOS.timeout(0.30)]
 
-def led16(self):
+def task16(self):
     ledpin16 = machine.Pin(15, machine.Pin.OUT)
     ledpin16.value(0)
     yield
@@ -147,21 +146,21 @@ def led16(self):
         ledpin16.toggle()
         yield [pyRTOS.timeout(0.25)]
 
-pyRTOS.add_task(pyRTOS.Task(led1, name="1"))
-pyRTOS.add_task(pyRTOS.Task(led2, name="2"))
-pyRTOS.add_task(pyRTOS.Task(led3, name="3"))
-pyRTOS.add_task(pyRTOS.Task(led4, name="4"))
-pyRTOS.add_task(pyRTOS.Task(led5, name="5"))
-pyRTOS.add_task(pyRTOS.Task(led6, name="6"))
-pyRTOS.add_task(pyRTOS.Task(led7, name="7"))
-pyRTOS.add_task(pyRTOS.Task(led8, name="8"))
-pyRTOS.add_task(pyRTOS.Task(led9, name="9"))
-pyRTOS.add_task(pyRTOS.Task(led10, name="10"))
-pyRTOS.add_task(pyRTOS.Task(led11, name="11"))
-pyRTOS.add_task(pyRTOS.Task(led12, name="12"))
-pyRTOS.add_task(pyRTOS.Task(led13, name="13"))
-pyRTOS.add_task(pyRTOS.Task(led14, name="14"))
-pyRTOS.add_task(pyRTOS.Task(led15, name="15"))
-pyRTOS.add_task(pyRTOS.Task(led16, name="16"))
+pyRTOS.add_task(pyRTOS.Task(task1))
+pyRTOS.add_task(pyRTOS.Task(task2))
+pyRTOS.add_task(pyRTOS.Task(task3))
+pyRTOS.add_task(pyRTOS.Task(task4))
+pyRTOS.add_task(pyRTOS.Task(task5))
+pyRTOS.add_task(pyRTOS.Task(task6))
+pyRTOS.add_task(pyRTOS.Task(task7))
+pyRTOS.add_task(pyRTOS.Task(task8))
+pyRTOS.add_task(pyRTOS.Task(task9))
+pyRTOS.add_task(pyRTOS.Task(task10))
+pyRTOS.add_task(pyRTOS.Task(task11))
+pyRTOS.add_task(pyRTOS.Task(task12))
+pyRTOS.add_task(pyRTOS.Task(task13))
+pyRTOS.add_task(pyRTOS.Task(task14))
+pyRTOS.add_task(pyRTOS.Task(task15))
+pyRTOS.add_task(pyRTOS.Task(task16))
 
 pyRTOS.start()
