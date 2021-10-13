678ba00da7e494d3c2cf8fbe0f9f554ef94253da
Luqmanul Hakim Zulkifli <49841356+mlhakimz@users.noreply.github.com>
2021-10-13 16:54:20 +0800

    Update pyRTOS_template.py

--- Example Code/MicroPython/pyRTOS/pyRTOS_template.py
+++ Example Code/MicroPython/pyRTOS/pyRTOS_template.py
@@ -1,12 +1,12 @@
 import pyRTOS
 
-def Task1(self):
-    # Task1 setup
+def task1(self):
+    # Task1 setup here
     yield
 
     while True:
-        # Task1 loop
+        # Task1 loop here
         yield [pyRTOS.timeout(1)]           # Delay in seconds (Other task can run)
         
-pyRTOS.add_task(pyRTOS.Task(led1))          # Add Task
+pyRTOS.add_task(pyRTOS.Task(task1))          # Add Task
 pyRTOS.start()                              # Start pyRTOS
