dd5b6a644c4602996b221a35449676d7e11e8e3f
Luqmanul Hakim Zulkifli <49841356+mlhakimz@users.noreply.github.com>
2021-11-01 19:09:12 +0800

    Update IoTRoomMonitoringDevice.py

--- Example Code/CircuitPython/pyRTOS/IoTRoomMonitoringDevice.py
+++ Example Code/CircuitPython/pyRTOS/IoTRoomMonitoringDevice.py
@@ -9,85 +9,85 @@ import adafruit_requests as requests
 import adafruit_espatcontrol.adafruit_espatcontrol_socket as socket
 from adafruit_espatcontrol import adafruit_espatcontrol
 
-temp = 0.0
-humidity = 0.0
-air = 0.0
-motion = 0
-sound = 0
+temp = 0.0                                      # Initialize global variables
+humidity = 0.0                                  # Initialize global variables
+air = 0.0                                       # Initialize global variables
+motion = 0                                      # Initialize global variables
+sound = 0                                       # Initialize global variables
 
 def DetectMotion(self):
-    PIR = digitalio.DigitalInOut(board.GP1)
-    PIR.direction = digitalio.Direction.INPUT
-    global motion
+    PIR = digitalio.DigitalInOut(board.GP1)     # Setup PIR sensor pin
+    PIR.direction = digitalio.Direction.INPUT   # Setup pin as digital input
+    global motion                               # Use the global variable
     yield
 
     while True:
-        if PIR.value:
-            motion = 1
-        yield [pyRTOS.timeout(0.1)]           # Delay in seconds (Other task can run)
+        if PIR.value:                           # If the PIR sensor detects motion
+            motion = 1                          # Update the global variable
+        yield [pyRTOS.timeout(0.1)]             # Delay in seconds (Other task can run)
 
 def DetectSound(self):
-    mic = digitalio.DigitalInOut(board.GP3)
-    mic.direction = digitalio.Direction.INPUT
-    global sound
+    mic = digitalio.DigitalInOut(board.GP3)     # Setup sound sensor pin
+    mic.direction = digitalio.Direction.INPUT   # Setup pin as digital input
+    global sound                                # Use the global variable
     yield
 
     while True:
-        if mic.value:
-            sound = 1
-        yield [pyRTOS.timeout(0.1)]           # Delay in seconds (Other task can run)
+        if mic.value:                           # If the sound sensor detects loud sound
+            sound = 1                           # Update the global variable
+        yield [pyRTOS.timeout(0.1)]             # Delay in seconds (Other task can run)
 
 def ReadDHT11(self):
-    dhtDevice = adafruit_dht.DHT11(board.GP5)
-    global temp
+    dhtDevice = adafruit_dht.DHT11(board.GP5)   # Setup DHTT11 on pin GP5
+    global temp                                 # Use the global variables
     global humidity
     yield
 
     while True:
         try:
-            temp = dhtDevice.temperature
+            temp = dhtDevice.temperature        # Update the global variables
             humidity = dhtDevice.humidity
         except RuntimeError as error:
-            # Errors happen fairly often, DHT's are hard to read, just keep going
+            # Errors happen fairly often,
+            # DHT's are hard to read, 
+            # just keep going
             print(error.args[0])
             continue
         except Exception as error:
             dhtDevice.exit()
             raise error
-        yield [pyRTOS.timeout(1)]           # Delay in seconds (Other task can run)
+        yield [pyRTOS.timeout(1)]               # Delay in seconds (Other task can run)
 
-def AirQuality(self):
-    airq = analogio.AnalogIn(board.A1)
-    conv = 330 / (65535)
-    global air
+def AirQuality(self):                           
+    airq = analogio.AnalogIn(board.A1)          # Setup air quality sensor on pin A1
+    conv = 330 / (65535)                        # Formula to convert analog value to percentage
+    global air                                  # Use the global variable
     yield
 
     while True:
-        air = round((airq.value * conv), 2)
-        yield [pyRTOS.timeout(0.2)]           # Delay in seconds (Other task can run)
+        air = round((airq.value * conv), 2)     # Round off the value to 2 decimal places and update
+        yield [pyRTOS.timeout(0.2)]             # Delay in seconds (Other task can run)
 
 def DisplayOLED(self):
-    i2c = busio.I2C(board.GP9, board.GP8)
-    oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
-    global temp
+    i2c = busio.I2C(board.GP9, board.GP8)               # Setup I2C pins with GP9 and GP8
+    oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)   # Setup OLED display using the above pins
+    global temp                                         # Use global variables
     global humidity
     global air
-    global motion
-    global sound
     yield
 
     while True:
-        oled.fill(0)
+        oled.fill(0)                             # Erase the OLED display
 
-        oled.text('Temperature = ', 0, 0, 1)
+        oled.text('Temperature = ', 0, 0, 1)     # Write the data
         oled.text(str(temp), 90, 0, 1)
         oled.text('Humidity = ', 0, 25, 1)
         oled.text(str(humidity), 90, 25, 1)
         oled.text('Air Pollution = ', 0, 50, 1)
         oled.text(str(air), 90, 50, 1)
 
-        oled.show()
-        yield [pyRTOS.timeout(0.5)]           # Delay in seconds (Other task can run)
+        oled.show()                              # Show the written data
+        yield [pyRTOS.timeout(0.5)]              # Delay in seconds (Other task can run)
 
 def SendData(self):
     global temp
@@ -115,6 +115,7 @@ def SendData(self):
     yield
 
     while True:
+        # Update the blynk datastream using HTTP GET requests
         requests.get("https://blynk.cloud/external/api/update?token=" + secrets["blynk_auth_token"] + "&v0=" + str(temp))
         yield [pyRTOS.timeout(0.5)]           # Let other tasks run
         requests.get("https://blynk.cloud/external/api/update?token=" + secrets["blynk_auth_token"] + "&v1=" + str(humidity))
@@ -125,6 +126,7 @@ def SendData(self):
         yield [pyRTOS.timeout(0.5)]           # Let other tasks run
         requests.get("https://blynk.cloud/external/api/update?token=" + secrets["blynk_auth_token"] + "&v4=" + str(sound))
         yield [pyRTOS.timeout(0.5)]           # Let other tasks run
+        # Reset the global variables
         if motion:
             motion = 0
         if sound:
